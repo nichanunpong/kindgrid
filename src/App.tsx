70e48d821e74657108835b5f5791a4959356ddea
Nichanun <nichanun.luck@gmail.com>
2025-10-25 13:56:46 -0700

    Fix: Add GitHub Pages routing support

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename='/kindgrid'>
         <Routes>
           <Route path='/' element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
